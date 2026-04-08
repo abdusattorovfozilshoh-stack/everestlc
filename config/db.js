923b0eb36d513e39468cf2ef46d9ba76baaf42d3
abdusattorovfozilshoh-stack <abdusattorovfozilshoh@gmail.com>
2026-04-08 23:37:37 +0500

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, '..', 'db.sqlite');
+const isVercel = process.env.VERCEL === '1';
+const dbPath = isVercel 
+    ? path.join('/tmp', 'db.sqlite') 
+    : path.join(__dirname, '..', 'db.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
